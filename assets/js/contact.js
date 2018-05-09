8dea332e2bd85e948b3fae347a0a19066b7ffb02
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-05-09 01:26:22 -0500

    Alternative way to send emails

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -31,20 +31,7 @@ function submitForm() {
     }
 }
 
-function showSnack(elementId) {
-    var x = document.getElementById(elementId);
-    if (x !== null) {
-        x.className = "snackbar show";
-        setTimeout(function () {
-            x.className = x.className.replace("snackbar show", "snackbar");
-        }, 3000);
-    }
-}
 
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email.toLowerCase());
-}
 
 function sendEmail(email, subject, body) {
     try{
