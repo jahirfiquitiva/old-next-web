d01e8e36526c94dee2a75b02b677dac9c8e70632
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-05-09 01:14:58 -0500

    Alternative way to send emails

---
+++ assets/js/emails.js
@@ -0,0 +1,28 @@
+const token = "4790479d-783a-42fc-bf9a-2835bbdb47d5";
+const $formContact = document.querySelector("#contact-form");
+
+const sendForm = event => {
+    event.preventDefault();
+    const message = {
+        sender: document.querySelector("#email").value,
+        name: document.querySelector("#form-name").value,
+        subject: document.querySelector("#form-subject").value,
+        text: document.querySelector("#form-message").value
+    };
+    smtpJS(message);
+};
+const smtpJS = message => {
+    try {
+        Email.send(
+            message.sender,
+            "hi@jahirfiquitiva.com",
+            message.subject,
+            `You've been contacted by ${message.name} \n\n ${message.text}`,
+            {token}
+        );
+    } catch (e) {
+        alert("Error! D:");
+    }
+};
+
+$formContact.addEventListener("submit", sendForm);
