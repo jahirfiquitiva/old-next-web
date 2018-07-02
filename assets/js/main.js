693ea9c3a91b94f7e29649c9322bdffc92a844e1
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-07-02 00:35:57 -0500

    :lipstick: Huge visual improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,35 @@ function loadParticles() {
                      });
 }
 
+function setup() {
+    let date = new Date();
+    let month = date.getMonth() + 1;
+    let logo = document.getElementById('logo');
+    let logoName = 'def';
+    // TODO: Add more month-styled logos
+    if (month === 6) {
+        logoName = '6';
+    }
+    logo.setAttribute('src', 'assets/img/brand/logo-' + logoName + '.svg');
+    logo.setAttribute('onerror',
+        'this.onerror=null;' +
+        'this.src=\'assets/img/brand/logo-' + logoName + '.png\'');
+
+    let copy = document.getElementById('copyright-notice');
+    copy.innerHTML = 'Copyright ' + date.getFullYear() + ' © All Rights Reserved';
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    let $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
         $navbarBurgers.forEach(function ($el) {
             $el.addEventListener('click', function () {
-
                 // Get the target from the "data-target" attribute
-                var target = $el.dataset.target;
-                var $target = document.getElementById(target);
+                let target = $el.dataset.target;
+                let $target = document.getElementById(target);
 
                 // Toggle the class on both the "navbar-burger" and the "navbar-menu"
                 $el.classList.toggle('is-active');
@@ -25,5 +42,6 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+    setup();
     loadParticles();
 });
