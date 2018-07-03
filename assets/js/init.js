e3affc6346acbf4daa3dbd96cf7d1ce381c7c94a
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-07-03 14:27:36 -0500

    :bug: Fix cursor icon

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,62 @@
+let useCharacterAsCursor = (function() {
+    let size = 24,
+        canvas,
+        context;
+
+    // Initialize canvas
+    canvas = document.createElement("canvas");
+    canvas.width = size;
+    canvas.height = size;
+
+    // Store context & set styling
+    context = canvas.getContext("2d");
+    context.font = size + "px sans-serif";
+    context.textBaseline = "middle";
+    context.textAlign = "center";
+
+    return function(character) {
+        if (!character) {
+            // If no character is provided, reset to default 
+            // to make sure there's a cursor shown
+            document.body.style.cursor = "default";
+            return;
+        }
+        // Clear previously drawn stuff (context is reused)
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        context.fillText(character, size / 2, size / 2);
+        let imgDataURL = canvas.toDataURL();
+
+        // Use offset to reposition the cursor 
+        // Two unit-less non-negative numbers less than 32
+        // (https://developer.mozilla.org/en-US/docs/Web/CSS/cursor)
+        // Use `auto` so the browser can still use the right cursor 
+        // for interactive elements
+        let offset = (size / 2) + " 0";
+        document.body.style.cursor = "url(" + imgDataURL + ") " + offset + ", auto";
+    };
+}());
+
+(function init() {
+    let textInput = document.querySelector(".js-textInput");
+    let selectInput = document.querySelector(".js-selectInput");
+
+    let onTextInputChange = function(event) {
+        selectInput.value = "custom";
+        useCharacterAsCursor(this.value);
+    };
+
+    let onSelectInputChange = function(event) {
+        textInput.value = "";
+        useCharacterAsCursor(this.value);
+    };
+
+    textInput.addEventListener("change", onTextInputChange);
+    selectInput.addEventListener("change", onSelectInputChange);
+
+    useCharacterAsCursor(selectInput.value);
+}());
+
 function setup() {
     let date = new Date();
     let month = date.getMonth() + 1;
@@ -47,4 +106,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     setup();
     loadParticles();
+    useCharacterAsCursor('🌀');
 });
