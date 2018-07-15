c0b6c669b1412ee8282aa02aaff91b512e889ce9
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-07-15 14:54:16 -0500

    :bug: Scroll fixes

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -17,9 +17,12 @@ window.addEventListener('scroll', function () {
 function getElementTop(itemId) {
     let elementTop = 0;
     if (itemId !== null && itemId !== undefined) {
-        let bodyRect = document.body.getBoundingClientRect(),
-            elemRect = document.getElementById(itemId).getBoundingClientRect();
-        elementTop = elemRect.top - bodyRect.top;
+        let element = document.getElementById(itemId);
+        let bodyRect = document.body.getBoundingClientRect();
+        if (element !== null && element !== undefined) {
+            let elemRect = element.getBoundingClientRect();
+            elementTop = elemRect.top - bodyRect.top;
+        }
         // elementTop = $("#" + itemId).offset().top;
         // elementTop = top - convertRemToPixels(7);
     }
