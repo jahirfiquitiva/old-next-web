e14dd3173181865a88a83968d71268f20b5584e4
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-06-09 00:19:39 -0500

    Update donation site to list supporters

--- assets/js/donate.js
+++ assets/js/donate.js
@@ -1,41 +1,57 @@
-function updateDonationLink(fire = false) {
-    let donationField = document.getElementById('donation-input');
-    let currencySelector = document.getElementById('currency-selector');
-    let donateBtn = document.getElementById('donate-btn');
-    if (donationField !== undefined && donationField !== null &&
-        currencySelector !== undefined && currencySelector !== null &&
-        donateBtn !== undefined && donateBtn !== null) {
-        let amount = 5;
-        let currency = "USD";
-        try {
-            amount = donationField.value;
-            currency = currencySelector.options[currencySelector.selectedIndex].value;
-        } catch (e) {
-            console.error(e)
-        }
-        donateBtn.href =
-            "https://www.paypal.me/jahirfiquitiva/" + amount.toString() +
-            currency.toString().toLowerCase();
-        if (fire) {
-            donateBtn.click();
-        }
-    }
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function launchDonationSite(event) {
-    event.preventDefault();
-    let code = 0;
-    if (event.key !== undefined) {
-        code = event.key;
-    } else if (event.keyIdentifier !== undefined) {
-        code = event.keyIdentifier;
-    } else if (event.keyCode !== undefined) {
-        code = event.keyCode;
+function loadSupporters() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', './assets/data/supporters.json', true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      try {
+        const supporters = JSON.parse(xhr.responseText);
+        if (supporters) {
+          const list = document.getElementById('supporters-list');
+          if (list) {
+            list.innerHTML = '';
+            let ind = 0;
+
+            for (const supp of supporters) {
+              const suppName = supp.name || '';
+              if (suppName.length <= 0) continue;
+
+              const suppLink = supp.link || '#';
+              const suppPhoto = supp.photo || '';
+
+              const aa = document.createElement('a');
+              aa.href = suppLink;
+              if (suppLink !== '#') aa.target = '_blank';
+              aa.classList.add('supporter');
+
+              const photoEl = document.createElement('span');
+              photoEl.classList.add('supporter-img');
+              photoEl.setAttribute('style', `background-color: ${getRandomColor()};`);
+              if (suppPhoto.length > 0) {
+                photoEl.setAttribute('style', `background-image: url('${suppPhoto}') !important;`);
+              } else {
+                photoEl.classList.add('mdi');
+                photoEl.classList.add('mdi-account');
+                photoEl.classList.add('mdi-24px');
+              }
+              aa.appendChild(photoEl);
+
+              const namep = document.createElement('p');
+              namep.innerHTML = `<b>${suppName}</b>`;
+              aa.appendChild(namep);
+
+              list.appendChild(aa);
+            }
+          }
+        }
+      } catch (e) {
+      }
     }
-    code = code.toString();
-    if (code === '13' || code === 'Enter' || code === 'enter') {
-        updateDonationLink(true);
-    }
+  };
+  xhr.send(null);
 }
 
-updateDonationLink();
+loadSupporters();
