1768a623dda816333af2d21dfeb7c2ea7ce37158
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-08-13 23:56:06 -0500

    Finish website

--- old/assets/js/scroll-listener.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-    let yPosition = (window.pageYOffset | document.body.scrollTop) + 2;
-    let projectsTop = getElementTop('projects');
-    if (yPosition < projectsTop) {
-        toggleLink('home');
-    } else {
-        toggleLink('projects');
-    }
-});
