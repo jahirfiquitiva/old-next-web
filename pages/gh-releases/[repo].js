1f0a587e67a3880cd9d452799e446c6288fdd5a9
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-06-27 23:52:58 -0500

    Update github releases site

---
+++ pages/gh-releases/[repo].js
@@ -0,0 +1,12 @@
+import Layout from '@components/Layout';
+import UnderConstruction from '@components/global/construction/construction';
+
+const Releases = () => {
+  return (
+    <Layout>
+      <UnderConstruction/>
+    </Layout>
+  );
+};
+
+export default Releases;
