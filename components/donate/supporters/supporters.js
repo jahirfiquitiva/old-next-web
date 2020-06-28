1e706b5d9f33b69892f8ba055bb5b3c362321a95
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-06-27 23:56:54 -0500

    Show names in supporters section

--- components/donate/supporters/supporters.js
+++ components/donate/supporters/supporters.js
@@ -1,11 +1,21 @@
 import data from './supporters.json';
 import styles from './supporters.module.css';
+import ExtLink from '@components/global/ext-link/ext-link';
 
 const Supporters = () => {
-  return (
-    <div className={styles.supporters}>
-      <h3 className={styles.title}>🙌&nbsp;&nbsp;Thanks!</h3>
-      <p>I'm really grateful to all the awesome people that support my work.</p>
+
+  const renderSupportersPhotos = () => {
+    return (<ul>
+      {(data || []).map((it, i) => {
+        return (
+          <li key={i}>
+            <ExtLink to={it.link} label={it.name}/>
+          </li>
+        );
+      })}
+    </ul>);
+    /*
+    return (
       <div className={styles.list}>
         {(data || []).map((it, i) => {
           return (
@@ -16,6 +26,15 @@ const Supporters = () => {
           );
         })}
       </div>
+    );
+     */
+  };
+
+  return (
+    <div className={styles.supporters}>
+      <h3 className={styles.title}>🙌&nbsp;&nbsp;Thanks!</h3>
+      <p>I'm really grateful to all the awesome people that support my work.</p>
+      {renderSupportersPhotos()}
     </div>
   );
 };
