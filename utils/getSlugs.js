564b773e1f1ffb0551e192f870d023072b35e511
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-07-07 19:34:50 -0500

    Don't generate sites for posts in progress

--- utils/getSlugs.js
+++ utils/getSlugs.js
@@ -1,9 +1,20 @@
+import matter from 'gray-matter';
+
 const getSlugs = (context) => {
   const keys = context.keys();
+  const values = keys.map(context);
   const data = keys.map((key, index) => {
-    return key.replace(/^.*[\\\/]/, '').slice(0, -3);
+    const slug = key.replace(/^.*[\\/]/, '')
+      .slice(0, -3);
+    const value = values[index];
+    const document = matter(value.default);
+    const frontmatter = { ...document.data };
+    const isInProgress = frontmatter['in-progress'] === true;
+    if (isInProgress) return null;
+    return slug;
   });
-  return data;
+
+  return (data || []).filter((it) => it);
 };
 
 export default getSlugs;
