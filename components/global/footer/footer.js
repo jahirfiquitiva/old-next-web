66f55024b42fc805ffedbabff1f8dc7c843b5946
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-07-09 09:21:43 -0500

    Improve footer links

--- components/global/footer/footer.js
+++ components/global/footer/footer.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import Icon from '@mdi/react';
 import { mdiHeart } from '@mdi/js';
 import Social from '@components/global/social/social';
-import ExtLink from '@components/global/ext-link/ext-link';
 import Logo from '@components/global/logo/logo';
 import styles from './footer.module.css';
 
@@ -10,8 +9,9 @@ const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.container}>
       <Social centered/>
-      <p>Made with <Icon path={mdiHeart} size={0.6}/> by <ExtLink to={'/'} label={'yours truly'}
-                                                                  newTab={false}/></p>
+      <p>
+        Made with <Icon path={mdiHeart} size={0.6}/> by <Link href={'/'}><a>yours truly</a></Link>
+      </p>
       <p>Copyright © {new Date().getFullYear()} | All Rights Reserved</p>
       <Link href={'/'}>
         <a className={styles.logito}>
