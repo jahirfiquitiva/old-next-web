dd8bfaa25bfbdd535b3f932785c4479cc1c2fa35
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-10-27 13:33:38 -0500

    Create redirects with next config instead of using nextjs-redirect component

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const buildRedirect = (source, destination, permanent = true) => {
+  return {
+    source,
+    destination,
+    permanent,
+  };
+};
+
 module.exports = {
   webpack(config) {
     config.module.rules.push({
@@ -15,5 +23,16 @@ module.exports = {
   },
   images: {
     domains: ['images.unsplash.com'],
+  },
+  async redirects() {
+    return [
+      buildRedirect('/dashbud', 'https://dashbud.dev'),
+      buildRedirect('/dashsetup', 'https://dashbud.dev'),
+      buildRedirect('/links', '/'),
+      buildRedirect('/resume', '/share/Jahir-Fiquitiva-Resume.pdf'),
+      buildRedirect('/support', '/donate'),
+      buildRedirect('/thanks', '/donate#thanks'),
+      buildRedirect('/uses', '/blog/uses'),
+    ];
   },
 };
