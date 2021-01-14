c912fb8cc099ec9b573fd11fa0f7761485e5892b
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-01-14 13:13:21 -0500

    Transform app to ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
+import { AppProps } from 'next/app';
+
 import { FormspreeProvider } from '@formspree/react';
 import ThemeWrapper from '@components/theme/ThemeWrapper';
 import '@components/global/styles/resetalize.min.css';
 import '@components/global/styles/global.css';
 
-export default function MyApp({ Component, pageProps }) {
+// noinspection JSUnusedGlobalSymbols
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <FormspreeProvider project={'1552841918776344307'}>
       <ThemeWrapper>
