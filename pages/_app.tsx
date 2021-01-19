f6ecb11ae1171ee962633bc10b8da9b345c6b295
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-01-18 20:42:05 -0500

    Remove formspree from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app';
-import { FormspreeProvider } from '@formspree/react';
 import ThemeWrapper from '@components/theme/ThemeWrapper';
 import '@components/global/styles/resetalize.min.css';
 import '@components/global/styles/global.css';
@@ -7,10 +6,8 @@ import '@components/global/styles/global.css';
 // noinspection JSUnusedGlobalSymbols
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <FormspreeProvider project={'1552841918776344307'}>
-      <ThemeWrapper>
-        <Component {...pageProps} />
-      </ThemeWrapper>
-    </FormspreeProvider>
+    <ThemeWrapper>
+      <Component {...pageProps} />
+    </ThemeWrapper>
   );
 }
