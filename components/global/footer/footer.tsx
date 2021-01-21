fe5d51dfdbf0994381551da8d8fbf35c0a9d9550
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-01-21 18:48:11 -0500

    Add translations for footer

--- components/global/footer/footer.tsx
+++ components/global/footer/footer.tsx
@@ -4,31 +4,41 @@ import { mdiHeart } from '@mdi/js';
 import Social from '@components/global/social/social';
 import Logo from '@components/global/logo/logo';
 import styles from './footer.module.css';
+import useTranslation from 'next-translate/useTranslation';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <div className={styles.container}>
-      <Social centered/>
-      <p>
-        {/* eslint-disable-next-line */}
-        Made with <Icon path={mdiHeart} size={0.6}/> by <Link href={'/'}><a>yours
-        truly</a></Link>
-      </p>
-      <p>Copyright © {new Date().getFullYear()} | All Rights Reserved</p>
-      <Link href={'/'}>
-        <a
-          title={'home link'} aria-label={'home link'}
-          className={styles.logito}>
-          {/*
+const Footer = () => {
+  const { t } = useTranslation('common');
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <Social centered/>
+        <p>
+          {/* eslint-disable-next-line */}
+          {t('made-with')}{' '}
+          <Icon path={mdiHeart} size={0.6}/>
+          {' '}{t('by')}{' '}
+          <Link href={'/'}><a>{t('yours-truly')}</a></Link>
+        </p>
+        <p>
+          {t('copyright')} © {new Date().getFullYear()}{' '}|
+          {' '}{t('all-rights-reserved')}
+        </p>
+        <Link href={'/'}>
+          <a
+            title={'home link'} aria-label={'home link'}
+            className={styles.logito}>
+            {/*
             <img src={`/assets/images/brand/logo-def${isDark ? '-dark' : ''}.svg`}
                  alt={'JF'} height={24} width={24} loading={'lazy'}/>
             */}
-          {/* @ts-ignore */}
-          <Logo className={styles.logosvg}/>
-        </a>
-      </Link>
-    </div>
-  </footer>
-);
+            {/* @ts-ignore */}
+            <Logo className={styles.logosvg}/>
+          </a>
+        </Link>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
