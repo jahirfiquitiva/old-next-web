b32463439068520d9d6d05c6af05244193ee9ccf
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-02-11 17:43:52 -0500

    Initial progress for getting subscribers from BMAC

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 const Fetcher = async (...args: Parameters<typeof fetch>) => {
   const res = await fetch(...args);
   return res.json();
--- pages/donate.tsx
+++ pages/donate.tsx
@@ -24,6 +24,24 @@ export default Donate;
 export const getStaticProps: GetStaticProps = async () => {
   const configData = await import('../siteconfig.json');
   const supportersData = await import('../data/supporters.json');
+
+  const res = await fetch(
+    'https://developers.buymeacoffee.com/api/v1/subscriptions?status=all', {
+      body: `access_token=${process.env.BMAC_API_KEY || ''}`,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      method: 'POST',
+    });
+  console.error('***************************************************');
+  if (res.status >= 200 && res.status < 300) {
+    console.log('Getting subscriptions ok');
+    const bmacSubscribers = await res.json();
+    console.log(bmacSubscribers);
+  } else {
+    console.error('Error getting BMAC subscribers');
+    console.error(`${res.status} -- ${res.statusText}`);
+  }
+  console.error('***************************************************');
+
   return {
     props: {
       title: `Donate ~ ${configData.default.title}`,
