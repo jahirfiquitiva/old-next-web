0ed815c6f011a48c0a955038e6d9f968788e9566
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-04-15 03:38:17 -0500

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
 const nextTranslate = require('next-translate');
 const { buildBlogPostsMatters } = require('./scripts/build-blog-posts-matters');
 
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src *.google.com *.twitter.com *.unsplash.com *.scdn.co;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+];
+
 const buildRedirect = (source, destination, permanent = true) => {
   return {
     source,
@@ -52,6 +95,18 @@ module.exports = nextTranslate({
   },
   images: {
     domains: ['images.unsplash.com', 'i.scdn.co', 'spotify.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: securityHeaders,
+      },
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
   async redirects() {
     const postsRedirects = await buildExternalBlogPostsRedirects()
